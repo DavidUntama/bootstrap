fca03dc78af093e5d914a80e0c7e61b6e6928b50
David Untama Huarachi <david.uhx64@gmail.com>
2017-08-03 20:05:08 -0500

    modificacion en el header usando bootstrap

--- index.php
+++ index.php
@@ -6,6 +6,16 @@
 	<?php include 'inc/head_common.php'; ?>
 </head>
 <body>
+	<div class="containerr">
+		<div class="row">
+			<div class="col-xs-3">
+				<h1><a href="#">WKSP<span>Workshops demo site</span></a></h1>
+			</div>
+			<div class="col-xs-9">
+				
+			</div>
+		</div>
+	</div>
 	<h1>bootstrap ejemplo</h1>
 	<p>Hola mundo</p>
 	<?php include 'inc/footer_common.php'; ?>
