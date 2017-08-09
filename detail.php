927084d9eabeaebc37a6f7565e0180fa07a40df6
David Untama Huarachi <david.uhx64@gmail.com>
2017-08-08 20:16:46 -0500

    intercambio index-detail y color de fondo oscuro

--- detail.php
+++ detail.php
@@ -2,12 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Trabajando con bootstrap</title>
+	<title>Talleres de info</title>
 	<?php include 'inc/head_common.php'; ?>
 </head>
 <body>
+	<div class="containerr">
+		<div class="row">
+			<div class="col-xs-3">
+				<h1><a href="#">WKSP<span>Workshops demo site</span></a></h1>
+			</div>
+			<div class="col-xs-9">
+				
+			</div>
+		</div>
+	</div>
 	<h1>bootstrap ejemplo</h1>
-	<p>Hola mundo</p>
 	<?php include 'inc/footer_common.php'; ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -2,21 +2,12 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>workshop primero</title>
+	<title>Trabajando con bootstrap</title>
 	<?php include 'inc/head_common.php'; ?>
 </head>
 <body>
-	<div class="containerr">
-		<div class="row">
-			<div class="col-xs-3">
-				<h1><a href="#">WKSP<span>Workshops demo site</span></a></h1>
-			</div>
-			<div class="col-xs-9">
-				
-			</div>
-		</div>
-	</div>
 	<h1>bootstrap ejemplo</h1>
+	<p>Hola mundo</p>
 	<?php include 'inc/footer_common.php'; ?>
 </body>
 </html>
